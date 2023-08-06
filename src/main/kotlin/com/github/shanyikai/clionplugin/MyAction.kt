97db6254315c6f0b019b6471dfe4855de46830df
GitHub Action <action@github.com>
2023-08-06 10:03:58 +0000

    Template cleanup

--- src/main/kotlin/com/github/shanyikai/clionplugin/MyAction.kt
+++ src/main/kotlin/com/github/shanyikai/clionplugin/MyAction.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.shanyikai.clionplugin
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
--- src/main/kotlin/com/github/shanyikai/clionplugin/MyBundle.kt
+++ src/main/kotlin/com/github/shanyikai/clionplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.shanyikai.clionplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
